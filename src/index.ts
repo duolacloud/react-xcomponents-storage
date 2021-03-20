039232236d03b0c9f36ff4ea2a0ef0f144f297d5
newtalentxp <newtalentxp@126.com>
2021-03-21 01:04:20 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Storage from './storage';
 
-import {} from './plugins/cos';
+export { COSStorage } from './plugins/cos';
 
 export { useUpload, Result } from './hooks';
 export default Storage;
